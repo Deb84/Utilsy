a414c30b32555838edd0d1075097877bdb330099
Deb84 <56758812+Deb84@users.noreply.github.com>
2025-11-05 20:57:30 +0100

    added unkown basics errors to discord errors

--- src/errors/discord/discord-errors.ts
+++ src/errors/discord/discord-errors.ts
@@ -1,6 +1,99 @@
 
 export class UnknownChannel extends Error {
-    constructor(msg?: string) {
-        super(msg)
+    result?: Result
+    channelId?: string
+
+    constructor(msg?: string, options?: { 
+        result?: Result,
+        channelId?: string,
+        errorOptions?: {cause?: Error},
+        }) {
+
+        super(msg || 'UnknownChannel', options?.errorOptions)
+        this.name = 'UnknownChannel'
+
+        this.result = options?.result
+        this.channelId = options?.channelId
+
+        Object.setPrototypeOf(this, UnknownChannel.prototype)
+    }
+}
+
+export class UnknownUser extends Error {
+    result?: Result
+    userId?: string
+
+    constructor(msg?: string, options?: { 
+        result?: Result,
+        userId?: string,
+        errorOptions?: {cause?: Error},
+        }) {
+
+        super(msg || 'Unknown User', options?.errorOptions)
+        this.name = 'UnknownUser'
+
+        this.result = options?.result
+        this.userId = options?.userId
+
+        Object.setPrototypeOf(this, UnknownUser.prototype)
+    }
+}
+
+export class UnknownGuild extends Error {
+    result?: Result
+    guildId?: string
+
+    constructor(msg?: string, options?: {
+        result?: Result,
+        guildId?: string,
+        errorOptions?: { cause?: Error },
+    }) {
+        super(msg || 'Unknown Guild', options?.errorOptions)
+        this.name = 'UnknownGuild'
+
+        this.result = options?.result
+        this.guildId = options?.guildId
+
+        Object.setPrototypeOf(this, UnknownGuild.prototype)
+    }
+}
+
+
+export class UnknownEmoji extends Error {
+    result?: Result
+    emojiId?: string
+
+    constructor(msg?: string, options?: {
+        result?: Result,
+        emojiId?: string,
+        errorOptions?: { cause?: Error },
+    }) {
+        super(msg || 'Unknown Emoji', options?.errorOptions)
+        this.name = 'UnknownEmoji'
+
+        this.result = options?.result
+        this.emojiId = options?.emojiId
+
+        Object.setPrototypeOf(this, UnknownEmoji.prototype)
+    }
+}
+
+
+export class UnknownRole extends Error {
+    result?: Result
+    roleId?: string
+
+    constructor(msg?: string, options?: {
+        result?: Result,
+        roleId?: string,
+        errorOptions?: { cause?: Error },
+    }) {
+        super(msg || 'Unknown Role', options?.errorOptions)
+        this.name = 'UnknownRole'
+
+        this.result = options?.result
+        this.roleId = options?.roleId
+
+        Object.setPrototypeOf(this, UnknownRole.prototype)
     }
 }
--- src/services/discord/discordInfos/modules/getChannel.ts
+++ src/services/discord/discordInfos/modules/getChannel.ts
@@ -1,4 +1,4 @@
-import { UnknownChannel } from "@/errors/discord/api/discordapi-errors.ts";
+import { UnknownChannel } from "@/errors/discord//discord-errors.ts";
 import { Channel, Client } from "discord.js";
 import * as R from 'result'
 
@@ -6,7 +6,7 @@ export default async (client: Client, channelId: string): Promise<Result<Channel
     try {
         const result = await client.channels.fetch(channelId, {allowUnknownGuild: true})
         if (result) return R.ok(result)
-        return R.err(new UnknownChannel())
+        return R.err(new UnknownChannel(undefined, {channelId: channelId}))
     } catch (e) {
         return R.err(e instanceof Error ? e : new Error(String(e)))
     }
